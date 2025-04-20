fb42b450256d59f80f1d85f614cf9f5af9b149db
Rikko Matsumato <kuttimsx@hotmail.com>
2025-04-20 17:38:28 +0300

    Update RunCMD.py

--- SrcCode/RunCMD.py
+++ SrcCode/RunCMD.py
@@ -1,10 +1,26 @@
-import subprocess
-from pathlib import Path
+from os import _exit as exitfunc
+import importlib.util as ut
 import os
-def run_command(cmd: list[str | Path]):
+from RunCMD import run_command
+from time import sleep
+def CheckIfFGI_IsInstalled():
     try:
-        print(f"running {cmd}")
-        return subprocess.run(cmd, stderr=subprocess.STDOUT, stdin=subprocess.STDOUT, stdout=subprocess.STDOUT)
+        return ut.find_spec("fgi")
+        
     except:
-        print(f"Failed to Run {cmd}!!!")
-        os._exit(334)
+        os.system("pip install git+https://github.com/commonuserlol/fgi")
+        print("Installed Successfully!!! Please Make Sure what Android NDK or Android Build Tools is Set as System Environment(Console Commands!!!)")
+        exitfunc(3221)
+
+def PatchAPK(apk_path: str, path_indexjs : str):
+    try:
+        CheckIfFGI_IsInstalled()
+        cmd_line = ["fgi", "-i", "{}.apk".format(apk_path), "-t script", "-l {}".format(path_indexjs),]
+        delimiter_space = " "
+        print(delimiter_space.join(cmd_line))
+        run_command(delimiter_space.join(cmd_line))
+
+    except:
+        print(f"Failed to Execute {cmd_line}")
+        sleep(14)
+        exitfunc(221)
