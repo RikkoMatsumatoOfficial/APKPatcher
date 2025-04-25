bd6d157c2b0a55952053ea8e28d262f0a471d99e
Rikko Matsumato <kuttimsx@hotmail.com>
2025-04-25 13:59:51 +0300

    Update PatchAPK.py

--- SrcCode/PatchAPK.py
+++ SrcCode/PatchAPK.py
@@ -1,6 +1,7 @@
 from os import _exit as exitfunc
 import importlib.util as ut
 import os
+import time
 def CheckIfFGI_IsInstalled():
     try:
         return ut.find_spec("fgi")
@@ -8,4 +9,5 @@ def CheckIfFGI_IsInstalled():
     except:
         os.system("pip install git+https://github.com/commonuserlol/fgi")
         print("Installed Successfully!!! Please Make Sure what Android NDK or Android Build Tools is Set as System Environment(Console Commands aka conhost!!!)")
+        time.sleep(12)
         exitfunc(3221)
